9f0f9fe7406bb49a07fd28e25299779ce7480ce4
jeffreyzhang <shine8606@gmail.com>
2012-07-07 18:01:16 +0800

    数据库连接

--- src/main/java/com/bb/diaries/database/DbManager.java
+++ src/main/java/com/bb/diaries/database/DbManager.java
@@ -1,43 +1,48 @@
 package com.bb.diaries.database;
 
-import java.sql.SQLException;
+import java.io.IOException;
+import java.util.Properties;
 
 import javax.sql.DataSource;
 
 import org.apache.commons.dbcp.BasicDataSource;
 import org.apache.commons.dbutils.QueryRunner;
-import org.apache.commons.dbutils.handlers.ArrayHandler;
+import org.apache.log4j.Logger;
 
 public class DbManager {
-    private static DataSource dataSource;
+	
+	private static Logger LOGGER = Logger.getLogger(DbManager.class);
+	
+	private static DataSource dataSource;
+	private static Properties jdbcProperties;
+    
+    static{
+    	jdbcProperties = new Properties();
+    	try {
+			jdbcProperties.load(DbManager.class.getResourceAsStream("jdbc.properteis"));
+		} catch (IOException e) {
+			e.printStackTrace();
+			LOGGER.error("加载数据库配置失败...");
+		}
+    }
+    
     private DbManager(){
     }
     
     public static QueryRunner getQueryRunner(){
         if(DbManager.dataSource==null){
-            //配置dbcp数据源
             BasicDataSource dbcpDataSource = new BasicDataSource();
-            dbcpDataSource.setUrl("jdbc:mysql://localhost:3306/bb_diaries?useUnicode=true&characterEncoding=UTF-8");
-            dbcpDataSource.setDriverClassName("com.mysql.jdbc.Driver");
-            dbcpDataSource.setUsername("root");
-            dbcpDataSource.setPassword("root");
+            dbcpDataSource.setUrl(jdbcProperties.getProperty("jdbc.url"));
+            dbcpDataSource.setDriverClassName(jdbcProperties.getProperty("jdbc.driver"));
+            dbcpDataSource.setUsername(jdbcProperties.getProperty("jdbc.username"));
+            dbcpDataSource.setPassword(jdbcProperties.getProperty("jdbc.password"));
             dbcpDataSource.setDefaultAutoCommit(true);
-            dbcpDataSource.setMaxActive(100);
-            dbcpDataSource.setMaxIdle(30);
-            dbcpDataSource.setMaxWait(500);
+            dbcpDataSource.setMaxActive(Integer.valueOf(jdbcProperties.getProperty("jdbc.MaxActive")));
+            dbcpDataSource.setMaxIdle(Integer.valueOf(jdbcProperties.getProperty("jdbc.MaxIdle")));
+            dbcpDataSource.setMaxWait(Integer.valueOf(jdbcProperties.getProperty("jdbc.MaxWait")));
             DbManager.dataSource = (DataSource)dbcpDataSource;
-            System.out.println("Initialize dbcp...");
         }
         return new QueryRunner(DbManager.dataSource);
     }
-	
-	public static void main(String[] args) {
-		QueryRunner qr = DbManager.getQueryRunner();
-		try {
-			Object[] result =  qr.query("select 1 from dual", new ArrayHandler());
-			System.out.println(result);
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-	}
+
 }
--- src/test/java/com/bb/diaries/database/DbManagerTest.java
+++ src/test/java/com/bb/diaries/database/DbManagerTest.java
@@ -1,5 +1,17 @@
 package com.bb.diaries.database;
 
-public class DbManagerTest {
+import java.sql.SQLException;
 
+import org.apache.commons.dbutils.QueryRunner;
+import org.apache.commons.dbutils.handlers.ArrayHandler;
+
+import junit.framework.TestCase;
+
+public class DbManagerTest extends TestCase {
+	
+	public void testConnection() throws SQLException {
+		QueryRunner qr = DbManager.getQueryRunner();
+		Object[] result = qr.query("select 1 from dual", new ArrayHandler());
+		assertEquals("1", result[0] + "");
+	}
 }
